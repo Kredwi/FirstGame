188fab2eb8407f33f9f0df9db5610665b5045baa
Kredwi <74652542+Kredwi@users.noreply.github.com>
2023-10-20 04:55:43 +0900

    New update
    New game mechanic
    New sound
    Optimization code
    Updated Styles

--- index.js
+++ index.js
@@ -2,22 +2,26 @@ const gameZone =  document.getElementById('gameZone');
 const gamePanel = document.getElementById('gamePanel');
 const item = document.getElementById('item');
 const player =  document.getElementById('player');
-const control = document.getElementById('controls')
-
-const killItem = new Audio('med.mp3');
+const control = document.getElementById('controls');
+const time = document.getElementById('time');
+const defeatMessage = document.getElementById('defeatMessage');
+const defeatBtn = document.getElementById('resetPlayerBtn');
 
 const pageWidth = gameZone.clientWidth;
 const pageHeight = gameZone.clientHeight;
 
-let step = 5;
-let isBall = localStorage.getItem('ball') || false;
+const killItem = new Audio('med.mp3');
+const deadPlayer = new Audio('eat.mp3');
+const playerRespawn = new Audio('playerRenaissance.mp3');
+
 let playerPosition = [0, 0];
 let gameMap = [pageHeight, pageWidth, 0, 0];
-let itemPosition = [0, 0, 0, 0];
+let itemPosition = [0, 0];
 let itemSize = 50;
+let step = 5;
+let gameTime = 60;
 let score = parseInt(localStorage.getItem('score')) ||  0;
-let interval;
-let moving;
+let interval, moving, defeatTime, isDead;
 
 gameZone.style.width = pageWidth;
 gameZone.style.height = pageHeight;
@@ -25,21 +29,53 @@ gameZone.style.height = pageHeight;
 randomSpawnItem(gameMap[0], gameMap[1], false);
 
 document.addEventListener("keydown", (event) => {
-    if (!moving)
-        movingPlayer(event.code);
+    if (!isDead) {
+        if (!moving)
+            movingPlayer(event.code);
+    }
 });
 
 control.addEventListener('mousedown', (event) => {
     interval = setInterval(() => {
         movingPlayer(event.target.id)
         moving = true;
-    }, 50)
+    }, 30)
 });
 
 control.addEventListener('mouseup', () => {
     clearInterval(interval)
     moving = false;
 });
+
+setInterval(() => {
+    if (!isDead) {
+        if (defeatTime == 0) {
+            setTimeout(() => {
+                deadPlayer.play();
+                player.style.top = '23px';
+                player.style.left = '64px';
+                setTimeout(() => {
+                    player.style.display = 'none';
+                    item.style.display = 'none';
+                    time.style.display = 'none';
+                    control.style.display = 'none';
+                    defeatMessage.style.display = 'block';
+                    defeatMessage.textContent = 'ВЫ ПРОИГРАЛИ!!!!!';
+                    defeatBtn.style.display = 'inline';
+                    defeatTime = 60;
+                    isDead = true;
+                }, 600);
+                player.style.transform = "scaleX(-1)";
+            }, 402);
+            setTimeout(() => {
+                resetButton();
+            }, 1000);
+        } else {
+            time.textContent = `Смерть через: ${defeatTime} сек`
+            defeatTime--;
+        }
+    }
+}, 1000);
 
 function movingPlayer(key) {
     if (key == "KeyW" && playerPosition[0] > gameMap[3]) {
@@ -62,9 +98,8 @@ function movingPlayer(key) {
         playerPosition[0] <= itemPosition[0] + itemSize &&
         playerPosition[1] >= itemPosition[1] - itemSize &&
         playerPosition[1] <= itemPosition[1] + itemSize
-        ) {
+    )
         randomSpawnItem(gameMap[0], gameMap[1], true);
-    }
 }
 
 function randomSpawnItem(Height, Width, from) {
@@ -78,6 +113,7 @@ function randomSpawnItem(Height, Width, from) {
     }
     document.getElementById('score').textContent = `СЧЕТ: ${score}`;
     localStorage.setItem('score', score);
+    defeatTime = gameTime;
     itemPosition[0] = oneCoordinate;
     itemPosition[1] = twoCoordinate;
 }
@@ -86,7 +122,42 @@ function resetScore() {
     const confirmDeleteScore = confirm('Точно удалить ваш счет?');
     if (confirmDeleteScore) {
         localStorage.removeItem('score');
+        defeatTime = gameTime;
+        isDead = false;
+        moving = false;
         location.reload();
-    } else
-        return;
+    }
+}
+
+function resetButton() {
+    playerPosition[0] = playerPosition[1] = 0;
+    player.style.top = '0px';
+    player.style.left = '0px';
+    clearInterval(interval);
+    defeatTime = gameTime;
+    moving = false;
+}
+
+function respawnPlayer() {
+    defeatMessage.textContent = 'ПОДОЖ';
+    setTimeout(() => {
+        defeatMessage.textContent = 'ПОДОЖДИТЕ...';
+    }, 1000);
+    setTimeout(() => {
+        defeatMessage.style.display = 'none';
+        defeatBtn.style.display = 'none';
+        setTimeout(() => {
+            player.style.display = 'inline';
+            item.style.display = 'inline';
+            time.style.display = 'inline';
+            control.style.display = 'inline-flex';
+            setTimeout(() => {
+                defeatMessage.textContent = '';
+                defeatTime = gameTime;
+                isDead = false;
+                moving = false;
+            }, 2000);
+        }, 3000);
+    }, 3000);
+    playerRespawn.play();
 }
